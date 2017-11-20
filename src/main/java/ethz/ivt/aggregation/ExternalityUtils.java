16bbd55628022facdc95318d816222b040b6c740
ctchervenkov <tchervec@ethz.ch>
2017-11-20 13:15:55 +0100

    new utils class for multiply used methods, ex. getTimeBin()

---
+++ src/main/java/ethz/ivt/aggregation/ExternalityUtils.java
@@ -0,0 +1,26 @@
+package ethz.ivt.aggregation;
+
+public class ExternalityUtils {
+    private ExternalityUtils(){}
+
+    public static int getTimeBin(double time, double binSize_s) {
+
+        double timeAfterSimStart = time;
+
+		/*
+		 * Agents who end their first activity before the simulation has started
+		 * will depart in the first time step.
+		 */
+        if (timeAfterSimStart <= 0.0) return 0;
+
+		/*
+		 * Calculate the bin for the given time. If the result
+		 * of the modulo operation is 0, it is the last time value
+		 * which is part of the previous bin.
+		 */
+        int bin = (int) (timeAfterSimStart / binSize_s);
+        if (timeAfterSimStart % binSize_s == 0.0) bin--;
+
+        return bin;
+    }
+}
