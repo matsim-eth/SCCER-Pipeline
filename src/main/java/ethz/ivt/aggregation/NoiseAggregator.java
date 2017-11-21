a7a92ef2d3fe8877dcbe6edb4cba69f37edb1bce
ctchervenkov <tchervec@ethz.ch>
2017-11-21 11:05:41 +0100

    use method from utils

--- src/main/java/ethz/ivt/aggregation/NoiseAggregator.java
+++ src/main/java/ethz/ivt/aggregation/NoiseAggregator.java
@@ -41,7 +41,7 @@ public class NoiseAggregator implements NoiseEventCausedHandler, LinkEnterEventH
         this.drivers = drivers;
 
         setUpBinsForLinks(scenario);
-        log.info("Number of congestion bins: " + num_bins);
+        log.info("Number of noise bins: " + num_bins);
     }
     
     protected void setUpBinsForLinks(Scenario scenario) {
@@ -59,36 +59,15 @@ public class NoiseAggregator implements NoiseEventCausedHandler, LinkEnterEventH
         });
     }
 
-    /*package*/ int getTimeBin(double time) {
-
-        double timeAfterSimStart = time;
-
-		/*
-		 * Agents who end their first activity before the simulation has started
-		 * will depart in the first time step.
-		 */
-        if (timeAfterSimStart <= 0.0) return 0;
-
-		/*
-		 * Calculate the bin for the given time. If the result
-		 * of the modulo operation is 0, it is the last time value
-		 * which is part of the previous bin.
-		 */  
-        int bin = (int) (timeAfterSimStart / binSize_s);
-        if (timeAfterSimStart % binSize_s == 0.0) bin--;
-
-        return bin;
-    }
-    
 	@Override
 	public void handleEvent(NoiseEventCaused event) {
-        int bin = getTimeBin(event.getTime());
+        int bin = ExternalityUtils.getTimeBin(event.getTime(), this.binSize_s);
         this.linkId2timeBin2values.get(event.getLinkId()).get("value")[bin] += event.getAmount();
 	}
 
 	@Override
 	public void handleEvent(LinkLeaveEvent event) {
-		int bin = getTimeBin(event.getTime());
+		int bin = ExternalityUtils.getTimeBin(event.getTime(), this.binSize_s);
 		Id<Person> pid = drivers.getDriverOfVehicle(event.getVehicleId());
         if(!this.linkId2timeBin2personId.get(event.getLinkId()).get(bin).contains(pid) ) {
         	this.linkId2timeBin2personId.get(event.getLinkId()).get(bin).add(pid);
@@ -98,7 +77,7 @@ public class NoiseAggregator implements NoiseEventCausedHandler, LinkEnterEventH
 
 	@Override
 	public void handleEvent(LinkEnterEvent event) {
-		int bin = getTimeBin(event.getTime());
+		int bin = ExternalityUtils.getTimeBin(event.getTime(), this.binSize_s);
 		Id<Person> pid = drivers.getDriverOfVehicle(event.getVehicleId());
         if(!this.linkId2timeBin2personId.get(event.getLinkId()).get(bin).contains(pid) ) {
         	this.linkId2timeBin2personId.get(event.getLinkId()).get(bin).add(pid);
