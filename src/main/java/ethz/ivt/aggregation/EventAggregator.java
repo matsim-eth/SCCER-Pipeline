145d233e35bfca08f5493dc152ef79dc39b3342a
ctchervenkov <tchervec@ethz.ch>
2017-11-21 10:40:06 +0100

    removed unused method

--- src/main/java/ethz/ivt/aggregation/EventAggregator.java
+++ src/main/java/ethz/ivt/aggregation/EventAggregator.java
@@ -67,27 +67,6 @@ public abstract class EventAggregator implements LinkEnterEventHandler, PersonDe
         this.setMethodsExecuted = false;
     }
 
-//    /*package*/ int getTimeBin(double time) {
-//
-//        double timeAfterSimStart = time;
-//
-//		/*
-//		 * Agents who end their first activity before the simulation has started
-//		 * will depart in the first time step.
-//		 */
-//        if (timeAfterSimStart <= 0.0) return 0;
-//
-//		/*
-//		 * Calculate the bin for the given time. Increase it by one if the result
-//		 * of the modulo operation is > 0. If it is 0, it is the last time value
-//		 * which is part of the previous bin.
-//		 */
-//        int bin = (int) (timeAfterSimStart / binSize_s);
-//        if (timeAfterSimStart % binSize_s == 0.0) bin--;
-//
-//        return bin;
-//    }
-
     @Override
     public void handleEvent(LinkEnterEvent event) {
         int bin = ExternalityUtils.getTimeBin(event.getTime(), this.binSize_s);
