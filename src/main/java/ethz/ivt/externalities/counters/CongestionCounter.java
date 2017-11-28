b5ea7d16054d61ec9282dc46bbb0527e974d8105
ctchervenkov <tchervec@ethz.ch>
2017-11-28 19:10:48 +0100

    integrated aggregate data objects to externality counters

--- src/main/java/ethz/ivt/externalities/counters/CongestionCounter.java
+++ src/main/java/ethz/ivt/externalities/counters/CongestionCounter.java
@@ -1,12 +1,7 @@
 package ethz.ivt.externalities.counters;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import ethz.ivt.externalities.ExternalityUtils;
+import ethz.ivt.externalities.data.AggregateCongestionData;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
@@ -15,23 +10,14 @@ import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.events.algorithms.Vehicle2DriverEventHandler;
 
-import com.opencsv.CSVReader;
-
 public class CongestionCounter extends ExternalityCounter {
 	private static final Logger log = Logger.getLogger(CongestionCounter.class);
-    protected Map<Id<Link>, double[]> linkId2timeBin2values = new HashMap<>();
+	private AggregateCongestionData aggregateCongestionData;
 
-	protected final int num_bins;
-	protected int binSize_s;
-
-    public CongestionCounter(Scenario scenario, Vehicle2DriverEventHandler drivers, String date, int binSize_s) {
+    public CongestionCounter(Scenario scenario, Vehicle2DriverEventHandler drivers, String date, AggregateCongestionData aggregateCongestionData) {
     	super(scenario, drivers, date);
-
-        this.num_bins = (int) (30 * 3600 / binSize_s);
-        this.binSize_s = binSize_s;
-
-        setUpBinsForLinks(scenario);
-        log.info("Number of congestion bins: " + num_bins);
+    	this.aggregateCongestionData = aggregateCongestionData;
+        log.info("Number of congestion bins: " + aggregateCongestionData.getNumBins());
     }
     
     @Override
@@ -40,58 +26,15 @@ public class CongestionCounter extends ExternalityCounter {
         keys.add("Delay");
     }
 
-    protected void setUpBinsForLinks(Scenario scenario) {
-        scenario.getNetwork().getLinks().keySet().forEach(l -> {
-            linkId2timeBin2values.put(l, new double[num_bins]);
-        });
-    }
-
-    public void loadCsvFile(String input) {
-    	CSVReader reader;
-		try {
-			reader = new CSVReader(new FileReader(input), ';');
-			// read line by line
-			String[] record = null;
-			int count = 0;
-			try {
-				while ((record = reader.readNext()) != null) {
-					if(count>0) {
-						Id<Link> lid = Id.createLinkId(record[0]);
-						int bin = Integer.parseInt(record[1]);
-						double delay = Double.parseDouble(record[2]);
-						this.linkId2timeBin2values.get(lid)[bin] = delay;
-					}
-					count ++;
-				}
-			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			try {
-				reader.close();
-			} catch (IOException e) {
-				log.error("Error while closing CSV file reader!");
-				e.printStackTrace();
-			}
-		} catch (FileNotFoundException e) {
-			log.error("CSV file not found!");
-			e.printStackTrace();
-		}
-    }
-
 	@Override
 	public void handleEvent(LinkEnterEvent event) {
-		int bin = ExternalityUtils.getTimeBin(event.getTime(), this.binSize_s);
+		int bin = ExternalityUtils.getTimeBin(event.getTime(), aggregateCongestionData.getBinSize());
 		Id<Link> lid = event.getLinkId();
         Id<Person> personId = drivers.getDriverOfVehicle(event.getVehicleId());
         if (personId == null) { //TODO fix this, so that the person id is retrieved properly
             personId = Id.createPersonId(event.getVehicleId().toString());
         }
-		double delay = this.linkId2timeBin2values.get(lid)[bin];
+		double delay = this.aggregateCongestionData.getMeanValue(lid, bin);;
 		double previous = this.tempValues.get(personId).get("Delay");
 		this.tempValues.get(personId).put("Delay", previous + delay);
 		
--- src/main/java/ethz/ivt/externalities/counters/NoiseCounter.java
+++ src/main/java/ethz/ivt/externalities/counters/NoiseCounter.java
@@ -1,34 +1,48 @@
 package ethz.ivt.externalities.counters;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import ethz.ivt.externalities.ExternalityUtils;
+import ethz.ivt.externalities.data.AggregateNoiseData;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.events.LinkEnterEvent;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.events.algorithms.Vehicle2DriverEventHandler;
 
 public class NoiseCounter extends ExternalityCounter {
 	private static final Logger log = Logger.getLogger(CongestionCounter.class);
-    protected Map<Id<Link>, double[]> linkId2timeBin2values = new HashMap<>();
+    private AggregateNoiseData aggregateNoiseData;
 
-	protected final int num_bins;
-	protected int binSize_s;
-
-	public NoiseCounter(Scenario scenario, Vehicle2DriverEventHandler drivers, String date, int binSize_s) {
+	public NoiseCounter(Scenario scenario, Vehicle2DriverEventHandler drivers, String date, AggregateNoiseData aggregateNoiseData) {
     	super(scenario, drivers, date);
-
-        this.num_bins = (int) (30 * 3600 / binSize_s);
-        this.binSize_s = binSize_s;
-
-        setUpBinsForLinks(scenario);
-        log.info("Number of congestion bins: " + num_bins);
+        this.aggregateNoiseData = aggregateNoiseData;
+        log.info("Number of congestion bins: " + aggregateNoiseData.getNumBins());
     }
 
-	private void setUpBinsForLinks(Scenario scenario) {
-        scenario.getNetwork().getLinks().keySet().forEach(l -> {
-            linkId2timeBin2values.put(l, new double[num_bins]);
-        });
-	}
+    @Override
+    protected void initializeFields() {
+        super.initializeFields();
+        keys.add("NoiseEmissions");
+    }
+
+    @Override
+    public void handleEvent(LinkEnterEvent event) {
+        int bin = ExternalityUtils.getTimeBin(event.getTime(), aggregateNoiseData.getBinSize());
+        Id<Link> lid = event.getLinkId();
+        Id<Person> personId = drivers.getDriverOfVehicle(event.getVehicleId());
+        if (personId == null) { //TODO fix this, so that the person id is retrieved properly
+            personId = Id.createPersonId(event.getVehicleId().toString());
+        }
+        double noise = this.aggregateNoiseData.getMeanValue(lid, bin);;
+        double previous = this.tempValues.get(personId).get("NoiseEmissions");
+        this.tempValues.get(personId).put("NoiseEmissions", previous + noise);
+
+        super.handleEvent(event); //add distance
+    }
+
+    public void writeCsvFile(String outputPath, String date) {
+        String outputFileName = date + "_noise.csv";
+        super.writeCsvFile(outputPath, outputFileName);
+    }
 }
