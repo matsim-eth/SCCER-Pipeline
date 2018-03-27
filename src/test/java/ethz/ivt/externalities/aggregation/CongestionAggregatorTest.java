8f5909155fb54f6072a101c4ad76b8176ae95506
ctchervenkov <tchervec@ethz.ch>
2018-03-27 19:19:56 +0200

    new updated tests for congestion aggregation per link per time

--- src/test/java/ethz/ivt/externalities/aggregation/CongestionAggregatorTest.java
+++ src/test/java/ethz/ivt/externalities/aggregation/CongestionAggregatorTest.java
@@ -1,5 +1,135 @@
+package ethz.ivt.externalities.aggregation;
+
+import ethz.ivt.externalities.data.AggregateCongestionDataPerLinkPerTime;
+import ethz.ivt.vsp.CongestionEvent;
+import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.LinkEnterEvent;
+import org.matsim.api.core.v01.events.LinkLeaveEvent;
+import org.matsim.api.core.v01.events.VehicleEntersTrafficEvent;
+import org.matsim.api.core.v01.events.VehicleLeavesTrafficEvent;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.events.algorithms.Vehicle2DriverEventHandler;
+import org.matsim.vehicles.Vehicle;
+
 import static org.junit.Assert.*;
 
 public class CongestionAggregatorTest {
 
+    @Test
+    public void handleCongestionEvent() {
+        Fixture fixture = new Fixture();
+        fixture.init();
+        Vehicle2DriverEventHandler v2deh = new Vehicle2DriverEventHandler();
+        AggregateCongestionDataPerLinkPerTime acd = new AggregateCongestionDataPerLinkPerTime(fixture.scenario, 3600.0);
+        CongestionAggregator ca = new CongestionAggregator(fixture.scenario,v2deh,acd);
+
+        // create events on single link
+        Id<Link> linkId = Id.create("0",Link.class);
+        CongestionEvent ce1 = new CongestionEvent(0.0, null,
+                Id.create("causingAgent",Person.class), Id.create("affectedAgent",Person.class),
+                1.0, linkId, 1.0);
+        CongestionEvent ce2 = new CongestionEvent(0.0, null,
+                Id.create("causingAgent",Person.class), Id.create("affectedAgent",Person.class),
+                1.0, linkId, 1.0);
+        CongestionEvent ce3 = new CongestionEvent(7220.0, null,
+                Id.create("causingAgent",Person.class), Id.create("affectedAgent",Person.class),
+                1.0, linkId, 7220.0);
+
+        // handle events
+        ca.handleEvent(ce1);
+        assertEquals("Incorrect delay value!",1.0,acd.getData().get(linkId).get("delay")[0],0.0);
+
+        ca.handleEvent(ce2);
+        assertEquals("Delay values not added correctly!",2.0,acd.getData().get(linkId).get("delay")[0],0.0);
+
+        ca.handleEvent(ce3);
+        assertEquals("Delay value not entered in correct bin!", 1.0,acd.getData().get(linkId).get("delay")[2],0.0);
+    }
+
+    @Test
+    public void testTwoPeopleEnterAndLeaveSameLink() {
+        Fixture fixture = new Fixture();
+        fixture.init();
+        Vehicle2DriverEventHandler v2deh = new Vehicle2DriverEventHandler();
+        AggregateCongestionDataPerLinkPerTime acd = new AggregateCongestionDataPerLinkPerTime(fixture.scenario, 3600.0);
+        CongestionAggregator ca = new CongestionAggregator(fixture.scenario,v2deh,acd);
+
+        // create events on single link
+        Id<Link> linkId0 = Id.create("0",Link.class);
+        Id<Person> pid1 = Id.createPersonId("person1");
+        Id<Person> pid2 = Id.createPersonId("person2");
+        Id<Vehicle> vid1 = Id.createVehicleId(pid1);
+        Id<Vehicle> vid2 = Id.createVehicleId(pid2);
+
+        // two people enter and leave same link
+
+        // new vehicle on link within time span, i.e count + 1
+        v2deh.handleEvent(new VehicleEntersTrafficEvent(0.0, pid1, linkId0, vid1, "car", 0.0));
+        LinkEnterEvent lee1 = new LinkEnterEvent(  0.0, vid1, linkId0);
+        ca.handleEvent(lee1);
+        assertEquals("First person enters link: Incorrect count value!", 1.0, acd.getValue(linkId0,0,"count"),0.0);
+
+        // same vehicle on link within time span, i.e count does not change
+        LinkLeaveEvent lle1 = new LinkLeaveEvent(100.0, vid1, linkId0);
+        ca.handleEvent(lle1);
+        assertEquals("First person leaves link: Incorrect count value!", 1.0, acd.getValue(linkId0,0,"count"),0.0);
+        v2deh.handleEvent(new VehicleLeavesTrafficEvent(100.0, pid1, linkId0, vid1, "car", 0.0));
+
+        // new vehicle on link within time span, i.e. count + 1
+        v2deh.handleEvent(new VehicleEntersTrafficEvent(100.0, pid2, linkId0, vid2, "car", 0.0));
+        LinkEnterEvent lee2 = new LinkEnterEvent(100.0, vid2, linkId0);
+        ca.handleEvent(lee2);
+        assertEquals("Second person enters link: Incorrect count value!", 2.0, acd.getValue(linkId0,0,"count"),0.0);
+
+        // same vehicle on link within time span, i.e count does not change
+        LinkLeaveEvent lle2 = new LinkLeaveEvent(200.0, vid2, linkId0);
+        ca.handleEvent(lle2);
+        assertEquals("Second person leaves link: Incorrect count value!", 2.0, acd.getValue(linkId0,0,"count"),0.0);
+        v2deh.handleEvent(new VehicleLeavesTrafficEvent(200.0, pid2, linkId0, vid2, "car", 0.0));
+
+    }
+
+    @Test
+    public void testLeaveFirstLinkAndEnterSecondLink() {
+        Fixture fixture = new Fixture();
+        fixture.init();
+        Vehicle2DriverEventHandler v2deh = new Vehicle2DriverEventHandler();
+        AggregateCongestionDataPerLinkPerTime acd = new AggregateCongestionDataPerLinkPerTime(fixture.scenario, 3600.0);
+        CongestionAggregator ca = new CongestionAggregator(fixture.scenario,v2deh,acd);
+
+        // create events on single link
+        Id<Link> linkId0 = Id.create("0",Link.class);
+        Id<Link> linkId1 = Id.create("1",Link.class);
+        Id<Person> pid1 = Id.createPersonId("person1");
+        Id<Vehicle> vid1 = Id.createVehicleId(pid1);
+
+        // person enter and exits two links
+
+        // vehicle enters link in previous time bin, i.e count[bin] + 1
+        v2deh.handleEvent(new VehicleEntersTrafficEvent(0.0, pid1, linkId0, vid1, "car", 0.0));
+        LinkEnterEvent lee1 = new LinkEnterEvent(  0.0, vid1, linkId0);
+        ca.handleEvent(lee1);
+        assertEquals("Person enters first link: Incorrect count value!", 1.0, acd.getValue(linkId0,0,"count"),0.0);
+
+        // vehicle leaves link in next time bin, i.e. count[bin] + 1
+        LinkLeaveEvent lle1 = new LinkLeaveEvent(3650.0, vid1, linkId0);
+        ca.handleEvent(lle1);
+        assertEquals("Person leaves first link: Incorrect count value!", 1.0, acd.getValue(linkId0,1,"count"),0.0);
+        v2deh.handleEvent(new VehicleLeavesTrafficEvent(3650.0, pid1, linkId0, vid1, "car", 0.0));
+
+        // vehicle enters new link within same time bin, i.e. count[bin] + 1
+        v2deh.handleEvent(new VehicleEntersTrafficEvent(3650.0, pid1, linkId1, vid1, "car", 0.0));
+        LinkEnterEvent lee2 = new LinkEnterEvent(3650.0, vid1, linkId1);
+        ca.handleEvent(lee2);
+        assertEquals("Person enters second link: Incorrect count value!", 1.0, acd.getValue(linkId1,1,"count"),0.0);
+
+        // same vehicle leaves link within time span, i.e count does not change
+        LinkLeaveEvent lle2 = new LinkLeaveEvent(4000.0, vid1, linkId0);
+        ca.handleEvent(lle2);
+        assertEquals("Person leaves second link: Incorrect count value!", 1.0, acd.getValue(linkId0,1,"count"),0.0);
+        v2deh.handleEvent(new VehicleLeavesTrafficEvent(4000.0, pid1, linkId0, vid1, "car", 0.0));
+
+    }
 }
---
+++ src/test/java/ethz/ivt/externalities/data/Fixture.java
@@ -0,0 +1,78 @@
+package ethz.ivt.externalities.data;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.population.routes.NetworkRoute;
+import org.matsim.core.population.routes.RouteUtils;
+import org.matsim.core.scenario.MutableScenario;
+import org.matsim.core.scenario.ScenarioUtils.ScenarioBuilder;
+import org.matsim.core.utils.misc.Time;
+import org.matsim.pt.transitSchedule.api.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Network:
+ * <pre>
+ *
+ * (n) node
+ *  l  link
+ *
+ *       0       1
+ *  (0)-----(1)-----(2)
+ *
+ *
+ * </pre>
+ * Coordinates: Distance 1 between nodes on axis.
+ *
+ *
+ * @author ctchervenkov
+ */
+/*package*/ class Fixture {
+
+    /*package*/ final MutableScenario scenario;
+    /*package*/ final Config config;
+    /*package*/ final Network network;
+    private final Node[] nodes = new Node[3];
+    private final Link[] links = new Link[2];
+
+    public Fixture() {
+        this.config = ConfigUtils.createConfig();
+        this.config.transit().setUseTransit(true);
+
+        ScenarioBuilder scBuilder = new ScenarioBuilder(config) ;
+        this.scenario = (MutableScenario) scBuilder.build() ;
+
+        this.network = this.scenario.getNetwork();
+    }
+
+    protected void init() {
+        buildNetwork();
+    }
+
+    protected void buildNetwork() {
+        this.nodes[0]  = this.network.getFactory().createNode(Id.create("0", Node.class), new Coord((double) 0, (double) 0));
+        this.nodes[1]  = this.network.getFactory().createNode(Id.create("1", Node.class), new Coord((double) 1, (double) 0));
+        this.nodes[2]  = this.network.getFactory().createNode(Id.create("2", Node.class), new Coord((double) 2, (double) 0));
+        for (int i = 0; i < nodes.length; i++) {
+            this.network.addNode(this.nodes[i]);
+        }
+
+        this.links[0]  = this.network.getFactory().createLink(Id.create( "0", Link.class), this.nodes[ 0], this.nodes[ 1]);
+        this.links[1]  = this.network.getFactory().createLink(Id.create( "1", Link.class), this.nodes[ 1], this.nodes[ 2]);
+        for (int i = 0; i < links.length; i++) {
+            this.links[i].setLength(1.0);
+            this.links[i].setFreespeed(1.0);
+            this.links[i].setCapacity(1.0);
+            this.links[i].setNumberOfLanes(1.0);
+            this.network.addLink(this.links[i]);
+        }
+    }
+}
