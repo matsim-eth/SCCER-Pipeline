fcd3af546dbac807380a4e4aca90f32e266bab47
ctchervenkov <tchervec@ethz.ch>
2018-03-28 15:12:58 +0200

    testing that a vehicle that re-enters a link during a same time bin gets counted again

--- src/test/java/ethz/ivt/externalities/aggregation/CongestionAggregatorTest.java
+++ src/test/java/ethz/ivt/externalities/aggregation/CongestionAggregatorTest.java
@@ -65,13 +65,13 @@ public class CongestionAggregatorTest {
 
         // two people enter and leave same link
 
-        // new vehicle on link within time span, i.e count + 1
+        // vehicle entering link within time span, i.e count + 1
         v2deh.handleEvent(new VehicleEntersTrafficEvent(0.0, pid1, linkId0, vid1, "car", 0.0));
         LinkEnterEvent lee1 = new LinkEnterEvent(  0.0, vid1, linkId0);
         ca.handleEvent(lee1);
         assertEquals("First person enters link: Incorrect count value!", 1.0, acd.getValue(linkId0,0,"count"),0.0);
 
-        // same vehicle on link within time span, i.e count does not change
+        // same vehicle exiting link within time span, i.e count does not change
         LinkLeaveEvent lle1 = new LinkLeaveEvent(100.0, vid1, linkId0);
         ca.handleEvent(lle1);
         assertEquals("First person leaves link: Incorrect count value!", 1.0, acd.getValue(linkId0,0,"count"),0.0);
@@ -107,13 +107,13 @@ public class CongestionAggregatorTest {
 
         // person enter and exits two links
 
-        // vehicle enters link in previous time bin, i.e count[bin] + 1
+        // vehicle enters link in previous time bin, i.e count[bin-1] + 1
         v2deh.handleEvent(new VehicleEntersTrafficEvent(0.0, pid1, linkId0, vid1, "car", 0.0));
         LinkEnterEvent lee1 = new LinkEnterEvent(  0.0, vid1, linkId0);
         ca.handleEvent(lee1);
         assertEquals("Person enters first link: Incorrect count value!", 1.0, acd.getValue(linkId0,0,"count"),0.0);
 
-        // vehicle leaves link in next time bin, i.e. count[bin] + 1
+        // vehicle leaves link in current time bin, i.e. count[bin] + 1
         LinkLeaveEvent lle1 = new LinkLeaveEvent(3650.0, vid1, linkId0);
         ca.handleEvent(lle1);
         assertEquals("Person leaves first link: Incorrect count value!", 1.0, acd.getValue(linkId0,1,"count"),0.0);
@@ -125,11 +125,51 @@ public class CongestionAggregatorTest {
         ca.handleEvent(lee2);
         assertEquals("Person enters second link: Incorrect count value!", 1.0, acd.getValue(linkId1,1,"count"),0.0);
 
-        // same vehicle leaves link within time span, i.e count does not change
+        // same vehicle leaves link within same time bin, i.e count does not change
         LinkLeaveEvent lle2 = new LinkLeaveEvent(4000.0, vid1, linkId0);
         ca.handleEvent(lle2);
         assertEquals("Person leaves second link: Incorrect count value!", 1.0, acd.getValue(linkId0,1,"count"),0.0);
         v2deh.handleEvent(new VehicleLeavesTrafficEvent(4000.0, pid1, linkId0, vid1, "car", 0.0));
 
+    }
+
+    @Test
+    public void testVehicleReentersLinkLaterInSameTimeBin() {
+        Fixture fixture = new Fixture();
+        fixture.init();
+        Vehicle2DriverEventHandler v2deh = new Vehicle2DriverEventHandler();
+        AggregateCongestionDataPerLinkPerTime acd = new AggregateCongestionDataPerLinkPerTime(fixture.scenario, 3600.0);
+        CongestionAggregator ca = new CongestionAggregator(fixture.scenario,v2deh,acd);
+
+        // create events on single link
+        Id<Link> linkId = Id.create("0",Link.class);
+        Id<Person> pid = Id.createPersonId("person1");
+        Id<Vehicle> vid = Id.createVehicleId(pid);
+
+        // person enters, exits, reenters and reexits link within same time bin
+
+        // vehicle enters link, i.e. count[bin] + 1
+        v2deh.handleEvent(new VehicleEntersTrafficEvent(0.0, pid, linkId, vid, "car", 0.0));
+        LinkEnterEvent lee1 = new LinkEnterEvent(  0.0, vid, linkId);
+        ca.handleEvent(lee1);
+        assertEquals("Person enters link: Incorrect count value!", 1.0, acd.getValue(linkId,0,"count"),0.0);
+
+        // vehicle leaves link, i.e. count does not change
+        LinkLeaveEvent lle1 = new LinkLeaveEvent(100.0, vid, linkId);
+        ca.handleEvent(lle1);
+        assertEquals("Person exits link: Incorrect count value!", 1.0, acd.getValue(linkId,0,"count"),0.0);
+        v2deh.handleEvent(new VehicleLeavesTrafficEvent(100.0, pid, linkId, vid, "car", 0.0));
+
+        // vehicle enters link, i.e. count[bin] + 1
+        v2deh.handleEvent(new VehicleEntersTrafficEvent(1000.0, pid, linkId, vid, "car", 0.0));
+        LinkEnterEvent lee2 = new LinkEnterEvent(  1000.0, vid, linkId);
+        ca.handleEvent(lee2);
+        assertEquals("Person re-enters link: Incorrect count value!", 2.0, acd.getValue(linkId,0,"count"),0.0);
+
+        // vehicle leaves link, i.e. count does not change
+        LinkLeaveEvent lle2 = new LinkLeaveEvent(1100.0, vid, linkId);
+        ca.handleEvent(lle2);
+        assertEquals("Person re-exits link: Incorrect count value!", 2.0, acd.getValue(linkId,0,"count"),0.0);
+        v2deh.handleEvent(new VehicleLeavesTrafficEvent(1100.0, pid, linkId, vid, "car", 0.0));
     }
 }
