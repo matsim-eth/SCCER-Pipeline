ff4f8f280cf43bb8131886c6d548e540f6ee8819
ctchervenkov <tchervec@ethz.ch>
2018-03-28 11:38:11 +0200

    adapted fixture for new test class (should be in previous commit)

--- src/test/java/ethz/ivt/externalities/data/Fixture.java
+++ src/test/java/ethz/ivt/externalities/data/Fixture.java
@@ -5,8 +5,12 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.Node;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.population.PersonUtils;
+import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.population.routes.RouteUtils;
 import org.matsim.core.scenario.MutableScenario;
@@ -40,8 +44,10 @@ import java.util.List;
     /*package*/ final MutableScenario scenario;
     /*package*/ final Config config;
     /*package*/ final Network network;
+    /*package*/ final Population population;
     private final Node[] nodes = new Node[3];
     private final Link[] links = new Link[2];
+    private final int numberPersons = 5;
 
     public Fixture() {
         this.config = ConfigUtils.createConfig();
@@ -51,10 +57,12 @@ import java.util.List;
         this.scenario = (MutableScenario) scBuilder.build() ;
 
         this.network = this.scenario.getNetwork();
+        this.population = this.scenario.getPopulation();
     }
 
     protected void init() {
         buildNetwork();
+        buildPopulation();
     }
 
     protected void buildNetwork() {
@@ -73,6 +81,14 @@ import java.util.List;
             this.links[i].setCapacity(1.0);
             this.links[i].setNumberOfLanes(1.0);
             this.network.addLink(this.links[i]);
+        }
+    }
+
+    protected void buildPopulation() {
+        for (int p = 0; p<numberPersons; p++) {
+            Id<Person> pid = Id.createPersonId(p);
+            Person person = this.population.getFactory().createPerson(pid);
+            this.population.addPerson(person);
         }
     }
 }
