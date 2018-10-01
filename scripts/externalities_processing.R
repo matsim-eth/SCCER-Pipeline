ddab038c339fbcf8347d600ec339bba206c661b4
Joseph Molloy <joe.m34@gmail.com>
2018-10-01 19:58:22 +0200

    started working on report generation code for feedback pdf, using python and Mako, which is pretty standard

--- scripts/externalities_processing.R
+++ scripts/externalities_processing.R
@@ -1,6 +1,3 @@
-install.packages("tidyverse")
-install.packages("zoo")
-install.packages("xtable")
 
 library(tidyverse)
 library(tidyr)
@@ -18,9 +15,13 @@ emissions <- read.csv("C:\\Projects\\SCCER_project\\output_gc\\gc_emissions.csv"
 congestion <- read.csv("C:\\Projects\\SCCER_project\\output_gc\\gc_congestion.csv", sep=";",
                        colClasses = c("integer", "Date", "integer", "numeric", "numeric", "numeric", 
                                       "character", 
+                                      "numeric", 
+                                      "numeric", 
+                                      "numeric", 
+                                      "numeric", 
                                       "numeric"))
 
-externalities = cbind(emissions, delay = congestion$Delay)
+externalities = cbind(emissions, delay = congestion$delay_experienced, matsim_delay = congestion$matsim_delay)
 
 externalities$duration = (externalities$EndTime - externalities$StartTime)
 externalities$avg_speed = externalities$Distance / (externalities$duration / 3.6)
@@ -30,7 +31,7 @@ save(externalities, file = "C:\\Projects\\SCCER_project\\output_gc\\gc_externali
 
 
 
-ext_filtered <- externalities %>% filter (avg_speed < 120 & duration > 60)
+ext_filtered <- externalities  %>% filter (avg_speed < 120 & duration > 60)
 
 ext_filtered %>% count / externalities %>% count
 
@@ -39,7 +40,7 @@ ext_filtered$Date_trunc <- as.Date(cut(ext_filtered$Date,  "day"))
 
 
 ##average speed of map matched traces
-hist(ext_filtered$avg_speed, main="", xlab="speed (km/h)", ylim=c(0,7000))
+hist(ext_filtered$avg_speed, main="", xlab="speed (km/h)")
 
 mode_agg <- ext_filtered %>% select(c(7:16)) %>% 
   mutate_all(funs(replace(., is.na(.), 0))) %>% 
@@ -54,7 +55,7 @@ ggplot(mode_agg) + geom_bar(aes(Pollutant, weight=value, fill=Mode))
 start_ecar_date <-  ext_filtered %>% filter(Mode == "Ecar") %>% slice(which.min(Date)) %>% select(Date)
 start_ecar_date <- as.Date(start_ecar_date$Date)
 
-per_date_count <- ext_filtered %>% group_by(Date_trunc, Mode) %>% tally(CO2.total.)
+per_date_count <- ext_filtered %>% group_by(Date_trunc, Mode) %>% summarize(n = sum(CO2.total., na.rm=T))
 
 
 pre_ecar_records <- per_date_count %>% filter(Mode == "Car" & Date_trunc < start_ecar_date & Date_trunc > as.Date("2016-11-15"))
@@ -120,8 +121,28 @@ ggplot(per_date_count %>% filter(Mode == "Car")) +
   geom_hline(yintercept = 1) + 
   geom_hline(yintercept = ecar_reduction_pc) + 
   theme_bw() +
-  theme(axis.text.x=element_text(angle=60, hjust=1), legend.position="none") +
+  theme(axis.text.x=element_text(angle=60, hjust=1, vjust=0.5), legend.position="none") +
   scale_fill_manual(values=c("#56B4E9", "#999999"))
+
+
+
+per_week_count <- ext_filtered %>% 
+  group_by(week = cut(Date_trunc, 'week'), Mode=as.factor(Mode)) %>% 
+  summarize(co2=sum(CO2.total., na.rm=T), Distance = sum(Distance, na.rm=T))
+
+per_week_distance <- ext_filtered %>% 
+  group_by(week = cut(Date_trunc, 'week')) %>% summarize(Distance = sum(Distance, na.rm=T))
+
+per_week_count$Mode <- factor(per_week_count$Mode, levels = rev(levels(per_week_count$Mode)))
+####plot of before and after Co2
+ggplot(per_week_count) + 
+  geom_area(aes(x=as.Date(week), y=co2/ 10e3, fill=Mode), stat="identity") +
+  xlab("") + ylab(expression(paste("Weekly kg of ", CO[2], " produced (grey)"))) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y" ) +
+  theme(axis.text.x=element_text(angle=60, hjust=1, vjust=0.5), legend.position="none") +
+  scale_fill_manual(values=c("sky blue", "dark grey", "black" )) +
+  geom_line(data=per_week_distance, mapping=aes(x=as.Date(week), y=Distance/max(Distance)*200)) 
+
 
 min5.congestion <- ext_filtered %>% mutate(avg.delay= delay/duration) %>%
   group_by( min5 = as.integer(StartTime) / (60)) %>% summarize(n =mean(avg.delay))
@@ -131,4 +152,8 @@ ggplot(min5.congestion) +
   geom_col(aes(x = min5, y=n)) 
 
 
+#######compare time lost and model:
+gc_congestion 
+ggplot(ext_filtered %>% filter (matsim_delay < duration)) + geom_point(aes(x=matsim_delay, y=delay))
 
+########flag trips where trip_time free_flow_time * 2
