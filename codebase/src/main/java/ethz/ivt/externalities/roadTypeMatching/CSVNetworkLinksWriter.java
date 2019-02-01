aad0b309c43b18eac26d758c78c61feeaed17e93
ctchervenkov <tchervec@ethz.ch>
2019-02-01 18:11:51 +0100

    write network with landuse csv

---
+++ codebase/src/main/java/ethz/ivt/externalities/roadTypeMatching/CSVNetworkLinksWriter.java
@@ -0,0 +1,53 @@
+package ethz.ivt.externalities.roadTypeMatching;
+
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.network.NetworkUtils;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class CSVNetworkLinksWriter {
+    private Network network;
+
+    public static void main(String[] args) throws IOException {
+        Network network = NetworkUtils.readNetwork(args[0]);
+        new CSVNetworkLinksWriter(network).write(args[1]);
+    }
+
+    public CSVNetworkLinksWriter(Network network) throws IOException {
+        this.network = network;
+    }
+
+    public void write(String outputPath) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputPath)));
+
+        writer.write(formatHeader() + "\n");
+        writer.flush();
+
+        for (Link item : this.network.getLinks().values()) {
+            writer.write(formatItem(item) + "\n");
+            writer.flush();
+        }
+
+        writer.flush();
+        writer.close();
+    }
+
+    private String formatHeader() {
+        return String.join(";", new String[] {
+                "linkId", "CH_BEZ_D"
+        });
+    }
+
+    private String formatItem(Link link) {
+        String urbanity =  "Ungebaut".equalsIgnoreCase((String) link.getAttributes().getAttribute("CH_BEZ_D")) ? "rural" : "urban";
+        return String.join(";", new String[] {
+                link.getId().toString(),
+                urbanity
+        });
+    }
+}
+
