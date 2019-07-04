a4ba749c5da4a247c51241d7baca3b661cb4d5b9
Joe Molloy <joe.m34@gmail.com>
2019-07-04 15:51:22 +0200

    run for all participants

--- codebase/src/main/scala/greenclass/SplitWaypoints.scala
+++ codebase/src/main/scala/greenclass/SplitWaypoints.scala
@@ -86,7 +86,7 @@ object SplitWaypoints {
         | -- and person_id in (select person_id from legs_per_person where days_since_first_leg > 27 and valid_dates >= 7)
         | and person_id in (select distinct participant_id from vehicle_information)
         | and id not in (select distinct (leg_id) from validation_externalities)
-        | and person_id <> '70057062'
+        | --and person_id = '70057062'
         |
         |order by person_id, leg_date, leg_id;
         |
