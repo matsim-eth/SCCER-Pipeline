0add80b9252758021d50ab9598442e564fdfd77e
Joseph Molloy <joe.m34@gmail.com>
2019-08-08 11:21:00 +0200

    fix mode mapping to matsim

--- data/sql1/get_motiontag_triplegs.sql
+++ data/sql1/get_motiontag_triplegs.sql
@@ -1,6 +1,6 @@
  SELECT 
 	user_id as person_id, 
-	id as leg_id, 
+	mt_trip_id as leg_id, 
 	started_at as leg_date, 
 	finished_at as end_date, 
 	mode  as mode_validated,
@@ -14,7 +14,7 @@
  where type='Track'
  -- and person_id in (select person_id from legs_per_person where days_since_first_leg > 27 and valid_dates >= 7)
  --and person_id in (select distinct participant_id from vehicle_information)
- and id not in (select distinct (leg_id) from externalities_list)
+ and mt_trip_id not in (select distinct (leg_id) from externalities_list)
  --and person_id = '70057062'
 
 order by person_id, leg_date, leg_id;
