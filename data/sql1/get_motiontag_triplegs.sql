2fa1badc04cf0d8880ae1759d503e0a6ff65aed1
Joe Molloy <joe.m34@gmail.com>
2019-09-29 23:01:04 +0200

    change to 10rp pt charge, handle merged trips properly

--- data/sql1/get_motiontag_triplegs.sql
+++ data/sql1/get_motiontag_triplegs.sql
@@ -10,14 +10,25 @@
 	ST_Y(ST_EndPoint(geometry)) as finish_y,
 	length as distance
 
- FROM motion_tag_trips as l
- where type='Track'
- and length / 1000  < 1000 --dont include trips longer than 1000km
- and not misdetected_completely
- -- and person_id in (select person_id from legs_per_person where days_since_first_leg > 27 and valid_dates >= 7)
- --and person_id in (select distinct participant_id from vehicle_information)
- -- and mt_trip_id not in (select distinct (leg_id) from externalities_list)
- and not overseas
-and user_id = 'BQCZK'
-
+ FROM (
+ 	 select 
+ 		ST_SETSRID(ST_MakeLine(array_agg((geom_part).geom order by (geom_part).path)), 4326) as geometry,
+ 		user_id, mt_trip_id, started_at, finished_at, mode, length
+	from (
+		 SELECT 
+			user_id, mt_trip_id, started_at, finished_at, mode, length,
+			ST_dump(geometry) as geom_part
+		 FROM motion_tag_trips as l
+		 where type='Track'
+		 and length / 1000  < 1000 --dont include trips longer than 1000km
+		 and not misdetected_completely
+		 and merged_into_id is null
+		 -- and person_id in (select person_id from legs_per_person where days_since_first_leg > 27 and valid_dates >= 7)
+		 --and person_id in (select distinct participant_id from vehicle_information)
+		 and mt_trip_id not in (select distinct (leg_id) from externalities_list)
+		 and not overseas
+	) a
+  	group by user_id, mt_trip_id, started_at, finished_at, mode, length
+) l
 order by person_id, leg_date, leg_id;
+
