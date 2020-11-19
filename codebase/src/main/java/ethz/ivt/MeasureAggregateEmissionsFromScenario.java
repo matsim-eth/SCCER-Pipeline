d624ddf7c231c4b672d5977db861ce21533c5f54
tchervec <christopher.tchervenkov@ivt.baug.ethz.ch>
2020-11-19 18:30:57 +0100

    wip

--- codebase/src/main/java/ethz/ivt/MeasureAggregateEmissionsFromScenario.java
+++ codebase/src/main/java/ethz/ivt/MeasureAggregateEmissionsFromScenario.java
@@ -1,125 +1,89 @@
 package ethz.ivt;
 
 import ethz.ivt.externalities.aggregation.EmissionsAggregator;
+import ethz.ivt.externalities.roadTypeMapping.HbefaRoadTypeMapping;
 import ethz.ivt.externalities.roadTypeMapping.OsmHbefaMapping;
 import org.apache.log4j.Logger;
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.TransportMode;
-import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.emissions.EmissionModule;
 import org.matsim.contrib.emissions.utils.EmissionsConfigGroup;
+import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsManagerImpl;
 import org.matsim.core.events.MatsimEventsReader;
 import org.matsim.core.events.algorithms.Vehicle2DriverEventHandler;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.vehicles.Vehicle;
-import org.matsim.vehicles.VehicleType;
-import org.matsim.vehicles.VehicleUtils;
 
-import java.util.Random;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MeasureAggregateEmissionsFromScenario {
     private final static Logger log = Logger.getLogger(MeasureExternalitiesFromTraceEvents.class);
 
-    private static String RUN_FOLDER; // = "/home/ctchervenkov/Documents/projects/road_pricing/switzerland_10pct";
-    private static String CONFIG_FILE; // = "switzerland_config_w_emissions.xml";
-    private static String EVENTS_FILE; // = "20.events.xml.gz";
+    private final Scenario scenario;
 
-    private Config config;
-    private EventsManagerImpl eventsManager;
+    public static void main(String[] args) throws CommandLine.ConfigurationException, IOException {
+        CommandLine cmd = new CommandLine.Builder(args)
+                .requireOptions("config-path", "vehicle-path", "events-path", "binsize", "output-path")
+                .build();
 
-    public static void main(String[] args) {
-//        RUN_FOLDER = args[0];
-//        CONFIG_FILE = args[1];
-//        EVENTS_FILE = args[2];
+        // set up scenario
+        String configPath = cmd.getOptionStrict("config-path");
+        String vehicleCompositionPath = cmd.getOptionStrict("vehicle-path");
 
-        RUN_FOLDER = "/home/ctchervenkov/Documents/projects/road_pricing/zurich_1pct/scenario/";
-        CONFIG_FILE = "defaultIVTConfig_w_emissions.xml";
-        EVENTS_FILE = "800.events.xml.gz";
-
-
-        new MeasureAggregateEmissionsFromScenario().run();
-    }
-
-    public void run() {
-
-        int bin_size_s = 3600;
-
-        config = ConfigUtils.loadConfig(RUN_FOLDER + CONFIG_FILE, new EmissionsConfigGroup());
-        config.controler().setOutputDirectory(RUN_FOLDER + "aggregate/");
+        // load config file and scenario
+        Config config = ConfigUtils.loadConfig(configPath, new EmissionsConfigGroup());
         Scenario scenario = ScenarioUtils.loadScenario(config);
 
-        //add mappings to network
-        OsmHbefaMapping.build().addHbefaMappings(scenario.getNetwork());
+        // adding hbefa mappings
+        log.info("Adding hbefa mappings");
+        HbefaRoadTypeMapping roadTypeMapping = OsmHbefaMapping.build();
+        roadTypeMapping.addHbefaMappings(scenario.getNetwork());
 
-        setUpVehicles(scenario);
+        // set up vehicle composition from file
+        VehicleGenerator vehicleGenerator = new VehicleGenerator(scenario);
+        vehicleGenerator.read(vehicleCompositionPath, 2015);
+        vehicleGenerator.setUpVehicles();
 
-        eventsManager = new EventsManagerImpl();
+        // process events
+        String eventsPath = cmd.getOptionStrict("events-path");
+        int binSize = Integer.parseInt(cmd.getOptionStrict("binsize"));
+        String outputPath = cmd.getOptionStrict("output-path");
+        new MeasureAggregateEmissionsFromScenario(scenario).run(eventsPath, binSize, outputPath);
+    }
 
+    public MeasureAggregateEmissionsFromScenario(Scenario scenario) {
+        this.scenario = scenario;
+    }
+
+    public void run(String eventsPath, int binSize, String outputPath) throws IOException {
+
+        // create event manager
+        EventsManagerImpl eventsManager = new EventsManagerImpl();
+
+        // add driver handler
         Vehicle2DriverEventHandler v2deh = new Vehicle2DriverEventHandler();
         eventsManager.addHandler(v2deh);
 
         // setup aggregators
-        EmissionModule emissionModule = new EmissionModule(scenario, eventsManager);
-        EmissionsAggregator emissionsAggregator = new EmissionsAggregator(scenario, v2deh);
+        EmissionModule emissionModule = new EmissionModule(this.scenario, eventsManager);
+
+        int maxTime = 30 * 3600;
+        Set<String> attributes = new HashSet<>(Arrays.asList("CO", "CO2(total)", "HC", "NMHC", "NOx", "NO2", "PM", "SO2"));
+
+        EmissionsAggregator emissionsAggregator = new EmissionsAggregator(binSize, maxTime, attributes);
         emissionModule.getEmissionEventsManager().addHandler(emissionsAggregator);
-        
+
         // read MATSim events
         MatsimEventsReader reader = new MatsimEventsReader(eventsManager);
-        reader.readFile(RUN_FOLDER + EVENTS_FILE);
+        reader.readFile(eventsPath);
 
         // save emissions data to csv files
-        emissionsAggregator.aggregateEmissionsDataPerLinkPerTime.writeDataToCsv(config.controler().getOutputDirectory() + "emissions/40pcdiesel/");
-        emissionsAggregator.aggregateEmissionsDataPerPersonPerTime.writeDataToCsv(config.controler().getOutputDirectory() + "emissions/40pcdiesel/");
-        log.info("Emissions aggregation completed for MATSim scenario " + CONFIG_FILE + ".");
-
-        eventsManager.finishProcessing();
-    }
-
-    public void reset() {
-        eventsManager.resetHandlers(0);
-    }
-
-    private void setUpVehicles(Scenario scenario) {
-        //householdid, #autos, auto1, auto2, auto3
-        //get household id of person. Assign next vehicle from household.
-
-        // Petrol car setup
-        VehicleType petrol_car = VehicleUtils.getFactory().createVehicleType(Id.create(TransportMode.car + "_petrol", VehicleType.class));
-        petrol_car.setMaximumVelocity(100.0 / 3.6);
-        petrol_car.setPcuEquivalents(1.0);
-        petrol_car.setDescription("BEGIN_EMISSIONSPASSENGER_CAR;petrol (4S);>=2L;PC-P-Euro-3END_EMISSIONS");
-        scenario.getVehicles().addVehicleType(petrol_car);
-
-        // Diesel car setup
-        VehicleType diesel_car = VehicleUtils.getFactory().createVehicleType(Id.create(TransportMode.car + "_diesel", VehicleType.class));
-        diesel_car.setMaximumVelocity(100.0 / 3.6);
-        diesel_car.setPcuEquivalents(1.0);
-        diesel_car.setDescription("BEGIN_EMISSIONSPASSENGER_CAR;diesel;<1,4L;PC D Euro-3END_EMISSIONS");
-        scenario.getVehicles().addVehicleType(diesel_car);
-
-        // percentage of diesel vehicles
-        Random randomGenerator = new Random();
-        double percentDiesel = 0.4;
-
-        for (Id<Person> pid : scenario.getPopulation().getPersons().keySet()) {
-            Id<Vehicle> vid = Id.createVehicleId(pid);
-
-            //add petrol or diesel vehicles according to percentage
-            double percent = randomGenerator.nextDouble();
-            if (percent < percentDiesel) {
-                Vehicle v = scenario.getVehicles().getFactory().createVehicle(vid, diesel_car);
-                scenario.getVehicles().addVehicle(v);
-            } else {
-                Vehicle v = scenario.getVehicles().getFactory().createVehicle(vid, petrol_car);
-                scenario.getVehicles().addVehicle(v);
-            }
-
-            //scenario.getHouseholds().popul  ().get(hid).getVehicleIds().add(vid);
-        }
+        emissionsAggregator.getAggregateEmissionsDataPerLinkPerTime().write(outputPath);
 
     }
+
 }
--- codebase/src/main/java/ethz/ivt/externalities/aggregation/EmissionsAggregator.java
+++ codebase/src/main/java/ethz/ivt/externalities/aggregation/EmissionsAggregator.java
@@ -4,18 +4,14 @@ import ethz.ivt.externalities.ExternalityUtils;
 import ethz.ivt.externalities.data.AggregateDataPerTimeImpl;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.emissions.events.ColdEmissionEvent;
 import org.matsim.contrib.emissions.events.ColdEmissionEventHandler;
 import org.matsim.contrib.emissions.events.WarmEmissionEvent;
 import org.matsim.contrib.emissions.events.WarmEmissionEventHandler;
-import org.matsim.core.events.algorithms.Vehicle2DriverEventHandler;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by molloyj on 06.10.2017.
@@ -23,48 +19,38 @@ import java.util.Map;
  */
 public class EmissionsAggregator implements WarmEmissionEventHandler, ColdEmissionEventHandler {
     private static final Logger log = Logger.getLogger(EmissionsAggregator.class);
-    private final Vehicle2DriverEventHandler drivers;
-    public AggregateDataPerTimeImpl<Link> aggregateEmissionsDataPerLinkPerTime;
-    public AggregateDataPerTimeImpl<Person> aggregateEmissionsDataPerPersonPerTime;
+    private AggregateDataPerTimeImpl<Link> aggregateEmissionsDataPerLinkPerTime;
 
-    public EmissionsAggregator(Scenario scenario, Vehicle2DriverEventHandler v2deh) {
-        this.drivers = v2deh;
-        ArrayList<String> attributes = new ArrayList<>(Arrays.asList("CO", "CO2(total)", "FC", "HC", "NMHC", "NOx", "NO2","PM", "SO2"));
-
-        this.aggregateEmissionsDataPerLinkPerTime = new AggregateDataPerTimeImpl<Link>(3600, attributes, Link.class);
-        this.aggregateEmissionsDataPerPersonPerTime = new AggregateDataPerTimeImpl<Person>(3600, attributes, Person.class);
+    public EmissionsAggregator(int binSize, int maxTime, Set<String> attributes) {
+        this.aggregateEmissionsDataPerLinkPerTime = new AggregateDataPerTimeImpl<Link>(binSize, maxTime, attributes, Link.class);
     }
 
     @Override
     public void handleEvent(ColdEmissionEvent event) {
         int timeBin = ExternalityUtils.getTimeBin(event.getTime(), aggregateEmissionsDataPerLinkPerTime.getBinSize());
-
-        Id<Person> personId = drivers.getDriverOfVehicle(event.getVehicleId());
         Id<Link> linkId = event.getLinkId();
 
         Map<String, Double> pollutants = event.getColdEmissions();
         for (Map.Entry<String, Double> p : pollutants.entrySet()) {
             String pollutant = p.getKey();
             aggregateEmissionsDataPerLinkPerTime.addValueToTimeBin(linkId, timeBin, pollutant, p.getValue());
-            aggregateEmissionsDataPerPersonPerTime.addValueToTimeBin(personId, timeBin, pollutant, p.getValue());
         }
     }
 
     @Override
     public void handleEvent(WarmEmissionEvent event) {
         int timeBin = ExternalityUtils.getTimeBin(event.getTime(), aggregateEmissionsDataPerLinkPerTime.getBinSize());
-
-        Id<Person> personId = drivers.getDriverOfVehicle(event.getVehicleId());
         Id<Link> linkId = event.getLinkId();
 
         Map<String, Double> pollutants = event.getWarmEmissions();
         for (Map.Entry<String, Double> p : pollutants.entrySet()) {
             String pollutant = p.getKey();
             aggregateEmissionsDataPerLinkPerTime.addValueToTimeBin(linkId, timeBin, pollutant, p.getValue());
-            aggregateEmissionsDataPerPersonPerTime.addValueToTimeBin(personId, timeBin, pollutant, p.getValue());
         }
-//        this.linkId2timeBin2values.get(event.getLinkId()).putIfAbsent("WarmEmissionsCount", new double[num_bins]);
-//        this.linkId2timeBin2values.get(event.getLinkId()).get("WarmEmissionsCount")[bin]++;
+    }
+
+    public AggregateDataPerTimeImpl<Link> getAggregateEmissionsDataPerLinkPerTime() {
+        return aggregateEmissionsDataPerLinkPerTime;
     }
 }
 
